187a6b96347d7439d44a8b74b6313bfd84e22893
hyunhaha <gusgk2658@naver.com>
2021-11-18 15:51:45 +0900

    feat: question 페이지 css 적용

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const Question = ({ data, onSelect }) => {
   const {
@@ -11,45 +12,90 @@ const Question = ({ data, onSelect }) => {
     qitemNo,
     question,
   } = data;
+  const [selectedItem, setSelectedItem] = useState("");
 
   const onAnswerSelect = e => {
     let question = qitemNo;
     let answer;
-    if (e.target.parentNode.className === "answer") {
+    console.log(e.target.parentNode.className);
+    if (e.target.parentNode.className === "answer ") {
+      console.log(e.target.parentNode.dataset.id);
       answer = Number(e.target.parentNode.dataset.score);
-    } else if (e.target.className === "answer") {
+      setSelectedItem(e.target.parentNode.dataset.id);
+    } else if (e.target.className === "answer ") {
+      console.log(e.target.dataset.id);
       answer = Number(e.target.dataset.score);
+      setSelectedItem(e.target.dataset);
     }
     onSelect(question, answer);
   };
 
   return (
-    <div>
-      <span>{`${qitemNo}. `}</span>
-      <span>{question}</span>
-      <div className="answer">
+    <SQuestionBlock>
+      <span className="question-number">{`${qitemNo}`}</span>
+      <h3>{question}</h3>
+      <div className="answer-set">
         <div
-          className="answer"
+          className={`answer ${selectedItem === "1" ? "selected" : ""}`}
+          data-id="1"
           data-score={answerScore01}
           onClick={onAnswerSelect}
-          style={{ border: "1px solid black" }}
         >
-          <div>{answer01}</div>
-          <div>{answer03}</div>
+          <h4 className="answer-title">{answer01}</h4>
+          <div className="answer-detail">{answer03}</div>
         </div>
 
         <div
-          className="answer"
+          className={`answer ${selectedItem === "2" ? "selected" : ""}`}
+          data-id="2"
           data-score={answerScore02}
           onClick={onAnswerSelect}
-          style={{ border: "1px solid black" }}
         >
-          <div>{answer02}</div>
-          <div>{answer04}</div>
+          <h4 className="answer-title">{answer02}</h4>
+          <div className="answer-detail">{answer04}</div>
         </div>
       </div>
-    </div>
+    </SQuestionBlock>
   );
 };
-
+const SQuestionBlock = styled.div`
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 6px;
+  margin-bottom: 10px;
+  position: relative;
+  .question-number {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    /* border: 1px solid; */
+    padding: 2px 6px;
+    border-radius: 4px;
+    background-color: gray;
+  }
+  .answer-set {
+    padding: 10px;
+    display: flex;
+  }
+  .answer {
+    flex: 1 1 0%;
+    border-radius: 6px;
+    cursor: pointer;
+    box-shadow: 0px 0px 5px 0px rgb(0 0 0 / 20%);
+  }
+  .answer:first-child {
+    margin-right: 10px;
+  }
+  .selected {
+    border: 1px solid red;
+  }
+  .answer-title {
+    padding: 10px 0;
+    margin: 0;
+  }
+  .answer-detail {
+    font-size: 12px;
+    padding: 0 30px 10px;
+  }
+`;
 export default Question;
