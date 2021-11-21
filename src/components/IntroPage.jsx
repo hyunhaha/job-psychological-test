ce5254f69196117e7c6b22b97cdfaebe7a6d4753
hyunhaha <gusgk2658@naver.com>
2021-11-21 23:48:04 +0900

    feat: introPage rerendering 버그 수정

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 import Question from "./Question";
@@ -6,16 +6,19 @@ import Question from "./Question";
 const IntroPage = props => {
   const navigator = useNavigate();
 
-  const data = {
-    question: "두 개 가치 중에 자신에게 더 중요한 가치를 선택하세요.",
-    answer01: "능력발휘",
-    answer02: "자율성",
-    answer03: "직업을 통해 자신의 능력을 발휘하는 것입니다.",
-    answer04: "일하는 시간과 방식에 대해서 스스로 결정할 수 있는 것입니다.",
-    answerScore01: "1",
-    answerScore02: "2",
-    qitemNo: 1,
-  };
+  const data = useMemo(() => {
+    return {
+      question: "두 개 가치 중에 자신에게 더 중요한 가치를 선택하세요.",
+      answer01: "능력발휘",
+      answer02: "자율성",
+      answer03: "직업을 통해 자신의 능력을 발휘하는 것입니다.",
+      answer04: "일하는 시간과 방식에 대해서 스스로 결정할 수 있는 것입니다.",
+      answerScore01: "1",
+      answerScore02: "2",
+      qitemNo: 1,
+    };
+  }, []);
+
   const [answer, setAnswer] = useState(0);
 
   const onSelect = (q, a) => {
