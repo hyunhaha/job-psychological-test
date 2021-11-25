5bfe61a75f8b1a285803361e86c5644618ed9ef2
hyunhaha <gusgk2658@naver.com>
2021-11-26 03:31:58 +0900

    feat: testCompleted페이지 style

--- src/components/TestCompletedPage.jsx
+++ src/components/TestCompletedPage.jsx
@@ -87,29 +87,50 @@ const TestCompletedPage = ({ userName }) => {
 
   return (
     <STestCompletedBlock>
-      <h1>검사가 완료되었습니다</h1>
-      <p>
-        검사결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게
-        생각하는지를 알려주고, 중요 가치를 충족시켜줄 수 있는 직업에 대해 생각해
-        볼 기회를 제공합니다.
-      </p>
-      <h2>
-        {userName ? userName : "사용자"}님는{" "}
-        {sortedResultScore.length !== 0 && part[sortedResultScore[0].key]}을
-        중요시 여기는 성향이므로 {matchJobs[0] && matchJobs[0][1]} 또는{" "}
-        {matchJobs[0] && matchJobs[1][1]}에 적합합니다.
-      </h2>
-      <Button disabled={false} onClick={onClickResult}>
-        결과보기
-      </Button>
+      <div>
+        <h1>검사가 완료되었습니다</h1>
+        <p>
+          검사결과는 여러분이 직업을 선택할 때 상대적으로 어떠한 가치를 중요하게
+          생각하는지를 알려주고, 중요 가치를 충족시켜줄 수 있는 직업에 대해
+          생각해 볼 기회를 제공합니다.
+        </p>
+        <SResult>
+          <SHighlight>{userName ? userName : "사용자"}</SHighlight>님는{" "}
+          <SHighlight>
+            {sortedResultScore.length !== 0 && part[sortedResultScore[0].key]}
+          </SHighlight>
+          을 중요시 여기는 성향이므로
+        </SResult>
+        <SResult>
+          <SHighlight>{matchJobs[0] && matchJobs[0][1]}</SHighlight> 또는{" "}
+          <SHighlight>{matchJobs[0] && matchJobs[1][1]}</SHighlight>에
+          적합합니다.
+        </SResult>
+        <Button disabled={false} onClick={onClickResult}>
+          결과보기
+        </Button>
+      </div>
     </STestCompletedBlock>
   );
 };
 const STestCompletedBlock = styled.div`
-  padding: 20px 10%;
+  padding: 20px 5%;
   text-align: center;
   width: 600px;
   margin: 0 auto;
   height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+`;
+
+const SResult = styled.p`
+  margin: 0;
+  font-size: 24px;
+  margin-bottom: 1rem;
+`;
+
+const SHighlight = styled.span`
+  font-weight: bold;
 `;
 export default TestCompletedPage;
