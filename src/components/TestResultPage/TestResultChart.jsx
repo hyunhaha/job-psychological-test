384be0d44f0a29575474959f1c1033d669c83854
hyunhaha <gusgk2658@naver.com>
2021-11-26 02:33:54 +0900

    feat: chart 스타일 추가

--- src/components/TestResultPage/TestResultChart.jsx
+++ src/components/TestResultPage/TestResultChart.jsx
@@ -1,19 +1,12 @@
 import React, { useEffect, useMemo } from "react";
+import styled from "styled-components";
+import TestResultChartBar from "./TestResultChartBar";
 
 const TestResultChart = ({ data }) => {
-  const interpretationNames = [
-    "능력발휘",
-    "자율성",
-    "보수",
-    "안정성",
-    "사회적 인정",
-    "사회봉사",
-    "자기계발",
-    "창의성",
-  ];
   useEffect(() => {
     console.log(data);
   }, [data]);
+
   const maxScore = useMemo(() => {
     if (Array.isArray(data)) {
       return Math.max(...data.map(({ score }) => score));
@@ -21,94 +14,39 @@ const TestResultChart = ({ data }) => {
     return 1;
   }, [data]);
 
-  const rows = useMemo(() => {
-    if (Array.isArray(data)) {
-      const row = (
-        <tr>
-          {data.map(() => (
-            <td>&nbsp;</td>
-          ))}
-        </tr>
-      );
-      const result = new Array(10).fill(row);
-      return result;
-    }
-    return [];
-  }, [data]);
   return (
-    <div
-      style={{
-        position: "relative",
-        width: "100%",
-        height: "auto",
-        marginBottom: 48,
-      }}
-    >
-      <table className="table table-bordered m-0">{rows}</table>
-      <div
-        style={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          left: 0,
-          bottom: 0,
-          display: "flex",
-          alignItems: "flex-end",
-          justifyContent: "space-between",
-        }}
-      >
+    <SChartBlock>
+      <SHeighstock />
+      <SBarWrap>
         {Array.isArray(data) &&
-          data.map(value => {
-            return (
-              <div
-                style={{
-                  flex: 1,
-                  display: "flex",
-                  alignItems: "flex-end",
-                  justifyContent: "center",
-                  height: "100%",
-                }}
-              >
-                <div
-                  className="bg-primary"
-                  style={{
-                    position: "relative",
-                    width: "80%",
-                    height: `${(value?.score / maxScore) * 80}%`,
-                    backgroundColor: "purple",
-                  }}
-                >
-                  <div
-                    className="text-secondary"
-                    style={{
-                      textAlign: "center",
-                      position: "absolute",
-                      bottom: "100%",
-                      width: "100%",
-                    }}
-                  >
-                    {value?.score}
-                  </div>
-
-                  <div
-                    className="text-secondary"
-                    style={{
-                      textAlign: "center",
-                      position: "absolute",
-                      top: "100%",
-                      width: "100%",
-                      paddingTop: 8,
-                    }}
-                  >
-                    {interpretationNames[value?.key - 1]}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    </div>
+          data.map((e, i) => (
+            <TestResultChartBar key={i} value={e} maxScore={maxScore} />
+          ))}
+      </SBarWrap>
+    </SChartBlock>
   );
 };
+
+const SChartBlock = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+  margin-bottom: 48px;
+`;
+
+const SHeighstock = styled.div`
+  height: 200px;
+`;
+
+const SBarWrap = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
 
 export default TestResultChart;
---
+++ src/components/TestResultPage/TestResultChartBar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const TestResultChartBar = ({ value, maxScore }) => {
+  const interpretationNames = [
+    "능력발휘",
+    "자율성",
+    "보수",
+    "안정성",
+    "사회적 인정",
+    "사회봉사",
+    "자기계발",
+    "창의성",
+  ];
+
+  return (
+    <SChartBarBlock>
+      <SBar score={value.score} maxScore={maxScore}>
+        <SValue>{value.score}</SValue>
+        <SAbility>{interpretationNames[value?.key - 1]}</SAbility>
+      </SBar>
+    </SChartBarBlock>
+  );
+};
+
+const SChartBarBlock = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  height: 100%;
+`;
+
+const SBar = styled.div`
+  position: relative;
+  width: 80%;
+  height: ${props => (props.score / props.maxScore) * 80}%;
+  background-color: #86a8e7;
+`;
+
+const SValue = styled.div`
+  position: absolute;
+  text-align: center;
+  bottom: 100%;
+  width: 100%;
+`;
+
+const SAbility = styled.div`
+  position: absolute;
+  text-align: center;
+  top: 100%;
+  width: 100%;
+  padding-top: 8px;
+`;
+
+export default TestResultChartBar;
