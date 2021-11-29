c426a85b8fffe8c73080270a6f1aa12ecd2cb872
hyunhaha <gusgk2658@naver.com>
2021-11-29 18:55:08 +0900

    feat: loading 추가 시도

--- src/components/TestCompletedPage/TestCompletedPage.jsx
+++ src/components/TestCompletedPage/TestCompletedPage.jsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
 import Button from "../commons/Button";
 import TestResultSummary from "./TestResultSummary";
 import { useTestState } from "../../provider/testProvider";
 import { setReportData } from "../../utils/settingData";
+import Loading from "../commons/Loading";
 
 const TestCompletedPage = props => {
   const navigate = useNavigate();
   const { state, dispatch } = useTestState();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     setReportData(dispatch, state);
+    setLoading(false);
   }, []);
   const onClickResult = () => {
     navigate("/result");
@@ -25,7 +29,8 @@ const TestCompletedPage = props => {
           생각하는지를 알려주고, 중요 가치를 충족시켜줄 수 있는 직업에 대해
           생각해 볼 기회를 제공합니다.
         </p>
-        <TestResultSummary />
+        {/* {loading ? <Loading /> : <TestResultSummary />} */}
+        {loading ? <Loading /> : <div>{state.user.name}</div>}
         <Button disabled={false} onClick={onClickResult}>
           결과보기
         </Button>
