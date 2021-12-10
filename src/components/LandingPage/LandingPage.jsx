29a50534a3a44979739f150882e194853d873345
hyunhaha <gusgk2658@naver.com>
2021-12-10 23:33:09 +0900

    feat: landing page 반응형적용

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,30 +6,40 @@ const LandingPage = props => {
   return (
     <SLandingPageBlock>
       <SWrap>
-        <SPart>
-          <h1>직업 가치관 검사</h1>
-        </SPart>
-        <SPart>
+        <h1>직업 가치관 검사</h1>
+        <div>
           <UserForm />
-        </SPart>
+        </div>
       </SWrap>
     </SLandingPageBlock>
   );
 };
-
+const BREAK_POINT_MOBILE = 768;
+const BREAK_POINT_TABLET = 992;
+const BREAK_POINT_PC = 1200;
 const SLandingPageBlock = styled.div`
   margin: 0 auto;
   box-sizing: border-box;
 `;
 const SWrap = styled.div`
   display: flex;
-  flex-direction: row;
   height: 100vh;
   align-items: center;
   box-sizing: border-box;
+  flex-direction: column;
+  justify-content: center;
+  @media only screen and (min-width: ${BREAK_POINT_MOBILE}px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+  @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+  @media only screen and (min-width: ${BREAK_POINT_PC}px) {
+    flex-direction: row;
+    justify-content: space-around;
+  }
 `;
-const SPart = styled.div`
-  flex: 1 1 0%;
-  text-align: center;
-`;
+
 export default LandingPage;
