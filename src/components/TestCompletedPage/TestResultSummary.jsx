961b0587d36f6d5c0af492cb082dd9976f032fdb
hyunhaha <gusgk2658@naver.com>
2021-12-16 22:21:58 +0900

    feat: 데이터가 있는지 확인 코드

--- src/components/TestCompletedPage/TestResultSummary.jsx
+++ src/components/TestCompletedPage/TestResultSummary.jsx
@@ -18,12 +18,13 @@ const TestResultSummary = props => {
     <div>
       <SResult>
         <SHighlight>{user.name}</SHighlight>
-        님는 <SHighlight>{abilityNames[sorted[0].key - 1]}</SHighlight>을 중요시
-        여기는 성향이므로
+        님는{" "}
+        <SHighlight>{sorted[0] && abilityNames[sorted[0].key - 1]}</SHighlight>
+        을 중요시 여기는 성향이므로
       </SResult>
       <SResult>
-        <SHighlight>{edu[0][1]}</SHighlight> 또는{" "}
-        <SHighlight>{major[0][1]}</SHighlight>에 적합합니다.
+        <SHighlight>{edu[0] && edu[0][1]}</SHighlight> 또는{" "}
+        <SHighlight>{major[0] && major[0][1]}</SHighlight>에 적합합니다.
       </SResult>
     </div>
   );
